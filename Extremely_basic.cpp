b7517c68df0d6a76b12ff7858f4ccd94763c5b8b
jim-bscse <mricto2420473@bscse.uiu.ac.bd>
2024-06-14 01:39:12 +0600

    beecrowd-1001solve

---
+++ Extremely_basic.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+    int A, B;
+    cin >> A;
+    cin >> B;
+
+    cout << "X = " << A+B << endl;
+
+    return 0;
+
+}
